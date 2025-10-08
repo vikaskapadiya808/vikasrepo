ce78e8ae2cfcea1f3995916274c2e3b3f52c0864
vikaskapadiya808 <vikaskapadiya808@gmail.com>
2025-10-08 16:54:04 +0530

    Update VasuApplication.java

--- vasuapp/vasuapp/src/main/java/com/vasu/VasuApplication.java
+++ vasuapp/vasuapp/src/main/java/com/vasu/VasuApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class VasuApplication {
+public class VasuApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VasuApplication.class, args);
